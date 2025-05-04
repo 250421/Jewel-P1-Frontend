5848baff977ecb61913c6a9931a335b0b7473b70
Rose Jewel <roseajewel@gmail.com>
2025-05-04 15:30:51 -0400

    added auth folder. signup page noto redirecting to dashboard

---
+++ jewel-p1-frontend/src/components/shared/navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <nav className="py-4 bg-green-300">
+            <ul>
+                <h1 className="font-bold text-2xl">Potion Inventory Management System</h1>
+            </ul>
+            <ul>
+                <h1>Profile</h1>
+            </ul>
+        </nav>
+    )
+}
